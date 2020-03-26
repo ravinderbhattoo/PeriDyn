d6c0654470bfb43e0b6b811369122b75e9b48802
Ravinder Bhattoo <ravinderbhattoo@gmail.com>
2020-03-26 11:23:40 +0530

    fast neigh, quasi static added

--- src/solvers.jl
+++ src/solvers.jl
@@ -64,3 +64,72 @@ function velocity_verlet!(envs::Any,N::Int64;freq1=10,freq2=50,file_prefix="data
         end
     end
 end
+
+
+function minimize!(env::GeneralEnv,step_size::Float64; max_iter::Int64=500,min_step_tol_per::Float64=5.0)
+    ref_particle_size = env.material_blocks[1].general.particle_size
+    orig_step_size = step_size
+    for i in 1:max_iter
+        update_acc!(env)
+        if maximum(abs.(env.f))*step_size < min_step_tol_per/100*ref_particle_size
+            step_size *= 2
+        elseif maximum(abs.(env.f))*step_size > ref_particle_size/2
+            step_size /= 2
+        else
+        end
+        env.y .+= step_size*env.f
+        for bc in env.boundary_conditions
+            apply_bc!(env,bc)
+        end
+        if step_size>1000*orig_step_size
+            break
+        end
+    end
+    env.time_step += 1
+end
+
+
+function quasi_static!(envs::Any,N::Int64,step_size::Float64; max_iter::Int64=100, min_step_tol_per::Float64=0.5, freq1::Int64=10, freq2::Int64=50, file_prefix::String="datafile",start_at::Int64=0)
+    mkpath("./output")
+    update_neighs!(envs)
+    N = N + start_at
+    for i in (1+start_at):N
+        for id in 1:size(envs,1)
+            if envs[id].state==2
+                fill!(env.v,0.0)
+                minimize!(envs[id],step_size)
+            end
+        end
+
+        if i%freq1==0.0 || i==1
+            print_data_file!(envs,file_prefix,i)
+            print(i/N*100,"% over\n")
+        end
+
+        if i%freq2==0.0
+            update_neighs!(envs)
+        end
+    end
+end
+
+function update_neighs!(envs)
+    print("\nUpdating neighbors for collision..................")
+    for env in envs
+        if env.state[1]==2
+            for rm in 1:size(env.short_range_repulsion,1)
+                update_repulsive_neighs!(env.y,env.type,env.short_range_repulsion[rm])
+            end
+        end
+    end
+    print("Done\n")
+end
+
+
+function print_data_file!(envs::Array{GeneralEnv}, file_prefix::String,i::Int64)
+    print("\nWritting data file................................")
+    for id in 1:size(envs,1)
+        env = envs[id]
+        write_data(string("./output/",file_prefix,"_env_",env.id,"_step_",i,".data"),env.type,env.y,env.v,env.f)
+    end
+    print("Done\n")
+end
--- src/test.jl
+++ src/test.jl
@@ -10,24 +10,24 @@ include("./materials/ordinary_state_based.jl")
 include("./contacts/contacts.jl")
 include("./contacts/simple.jl")
 include("./util.jl")
-include("./solvers.jl")
 include("./global_bc.jl")
+include("./solvers.jl")
 
-x1 = create_block([1.0,1,1],[100,10,10])
-# notch_mask = (48 .< x1[1,:] .< 52) .& (x1[2,:].<10)
-# x1 = x1[:,.~notch_mask]
+x1 = create_block([1.0,1,1],[40,20,10])
+notch_mask = (19 .< x1[1,:] .< 23) .& ((x1[2,:].<6) .| (x1[2,:].>15))
+x1 = x1[:,.~notch_mask]
 
 v1 = x1*0
 y1 = 1x1
 
-mat_gen1 = GeneralMaterial(y1,v1,x1,x1[1,:]*0 .+ 1, 1000.0, 3.0, 0.15, max_neigh=200)
+mat_gen1 = GeneralMaterial(y1,v1,x1,x1[1,:]*0 .+ 1, 1000.0, 3.0, 0.5, max_neigh=200)
 mat_spec1 = OrdinaryStateBasedSpecific(10.0,10.0,mat_gen1)
 block1 = OrdinaryStateBasedMaterial(1,mat_gen1,mat_spec1)
 
 bool1 = y1[1,:].<10
-bool2 = y1[1,:].>90
+bool2 = y1[1,:].>30
 BC1 = Move_BC(1,bool1,[0.0,0,0],1y1[:,bool1])
-BC2 = Move_BC(1,bool2,[0.01,0,0],1y1[:,bool2])
+BC2 = Move_BC(1,bool2,[.5,0,0],1y1[:,bool2])
 
 
 lazzy_mask = (40 .< x1[1,:] .< 60)
@@ -35,10 +35,11 @@ RM1 = SimpleRepulsionModel(2.0,1.0,block1,distanceX=3,max_neighs=200,)
 
 env =  Env(1,[block1],[RM1],[BC1,BC2],1)
 
-@time velocity_verlet!([env],5000,freq1=20,freq2=20)
 
-@time velocity_verlet_step!(env)
+quasi_static!([env],50,10.0,freq1=1,freq2=1,file_prefix="minimize",start_at=0)
 
-@time update_repulsive_neighs!(env.y,env.type,RM1)
+
+
+
 
 #
