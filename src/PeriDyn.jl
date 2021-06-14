87958bef5882cea46e5de58b6c27895eb75625f6
Ravinder Bhattoo <ravinderbhattoo@gmail.com>
2021-06-15 01:36:52 +0530

    mesh and testtool removed

--- src/PeriDyn.jl
+++ src/PeriDyn.jl
@@ -1,6 +1,5 @@
 module PeriDyn
 
-include("./mesh.jl") # utility fuctions to create mesh
 include("./simulation.jl") # define simulation environment
 include("./operators.jl") # standars peridynamic operators (redundant)
 
@@ -13,9 +12,7 @@ include("./materials/ordinary_state_based.jl")
 include("./contacts/contacts.jl")
 
 include("./util.jl") # utility functions
-include("./global_bc.jl") # boundary condition functions
+include("./boundary_conditions.jl") # boundary condition functions
 include("./solvers.jl") # All implemented solvers
-
-include("./testtools.jl") # only for testing
 
 end
