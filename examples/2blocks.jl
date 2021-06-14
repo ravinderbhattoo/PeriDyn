8a358c779e5d877e9135348a3216e5be16959070
Ravinder Bhattoo <ravinderbhattoo@gmail.com>
2021-06-15 01:38:31 +0530

    example updated

--- examples/2blocks.jl
+++ examples/2blocks.jl
@@ -1,51 +1,41 @@
 using Revise
 using PeriDyn
+using PDMesh
 
 
-x1, v1, y1, vol1 = create_block([1.0,1,1],[100,100,10])
-den1 = 1000.0
+x1, v1, y1, vol1, type1 = create(Cuboid([0 10; 0 10; 0 10]), resolution=1, type=1)
+
 hor1 = 3.0
-cstretch1 = 0.15
-
-mat_gen1 = GeneralMaterial(y1, v1, x1, vol1, den1, hor1, cstretch1, max_neigh=200)
+mat_gen1 = GeneralMaterial(y1, v1, x1, vol1, type1, hor1, max_neigh=200)
 
 K, G = 1.0, 1.0
-mat_spec1 = OrdinaryStateBasedSpecific(K,G,mat_gen1)
+den1 = 1000.0
+cstretch1 = 0.5
+mat_spec1 = OrdinaryStateBasedSpecific([K], [G], [cstretch1], [den1])
 
-id = 1::Int64
-block1 = OrdinaryStateBasedMaterial(id, mat_gen1, mat_spec1)
+block1 = PeridynamicsMaterial(mat_gen1, mat_spec1)
 
-x2, v2, y2, vol2 = create_block([1.0,1,1],[10,10,10])
+x2, v2, y2, vol2, type2 = create(slide(Cuboid([0 10; 0 10; 0 10]), by=[0.0, 0, 10.2]), resolution=1, type=2)
 
-x2[1,:] .+= 10.5
-x2[2,:] .+= 20.5
-x2[3,:] .+= 20.5
+v2[3,:] .-= 0.001
 
-v2[1,:] .-= 0.01
-
-den2 = 10000.0
 hor2 = 3.0
+mat_gen2 = GeneralMaterial(y2, v2, x2, vol2, type2, hor2, max_neigh=200)
+
+K2, G2 = 1.0, 1.0
+den2 = 1000.0
 cstretch2 = 0.5
+mat_spec2 = OrdinaryStateBasedSpecific([K2], [G2], [cstretch2], [den2])
 
-mat_gen2 = GeneralMaterial(y2, v2, x2, vol2, den2, hor2, cstretch2, max_neigh=200)
+block2 = PeridynamicsMaterial(mat_gen2, mat_spec2)
 
-K2, G2 = 10.0, 10.0
+k = 1000.0
+RM = LinearRepulsionModel(k, block1, block2, distanceX=2, max_neighs=200)
+RM1 = LinearRepulsionModel(k, block1, distanceX=2, max_neighs=200)
+RM2 = LinearRepulsionModel(k, block2, distanceX=2, max_neighs=200)
 
-mat_spec2 = OrdinaryStateBasedSpecific(K2, G2, mat_gen2)
+env = Env(1, [block1, block2], [RM RM1 RM2], [], 0.2)
 
-id = 2::Int64
-block2 = OrdinaryStateBasedMaterial(id, mat_gen2, mat_spec2)
-
-
-alpha = 2.0
-epsilon = 1000.0
-
-RM = SimpleRepulsionModel(alpha, epsilon, block1, block2, distanceX=2, max_neighs=200)
-RM1 = SimpleRepulsionModel(alpha, epsilon, block1, distanceX=3, max_neighs=200)
-RM2 = SimpleRepulsionModel(alpha, epsilon, block2, distanceX=2, max_neighs=200)
-
-env = Env(1, [block1, block2], [], [], 0.2)
-
-velocity_verlet!([env],1000,filewrite_freq=20,neigh_update_freq=20,file_prefix="2blocks",start_at=0)
+velocity_verlet!([env], 5000, filewrite_freq=20, neigh_update_freq=20, file_prefix="2blocks", start_at=0)
 
 #
