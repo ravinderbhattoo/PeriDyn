332d2391182775173510fb9832fd766b5e83c9fb
Ravinder Bhattoo <ravinderbhattoo@gmail.com>
2022-11-05 19:58:04 +0530

    Docs updated

--- documenter/make.jl
+++ documenter/make.jl
@@ -1,13 +1,18 @@
-using Documenter, DocThemeIndigo
+push!(LOAD_PATH,"../src/")
+
+using Documenter #DocThemeIndigo
 using PeriDyn, PDMesh
 
-indigo = DocThemeIndigo.install(PeriDyn)
+# indigo = DocThemeIndigo.install(PeriDyn)
 
-makedocs(sitename="PeriDyn", 
-        modules=[PeriDyn, PDMesh],
+makedocs(sitename="PeriDyn",
+        build = "../docs",
         format = Documenter.HTML(;
-            prettyurls = false,
-            assets=String[indigo]),
+            prettyurls = get(ENV, "CI", nothing) == "true",
+            assets=["indigo.css"]
+            ),
+        # format = Documenter.LaTeX(),
+        modules=[PeriDyn, PDMesh],
         pages = [
             "Home" => "peridyn.md",
             "Table of contents" => "toc.md",
